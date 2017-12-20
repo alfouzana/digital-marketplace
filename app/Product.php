cc61edfd1ab85f1f5c6d89c9d4b5907c1f51b1bf
Mohammad Ali Tavassoli <mtvs.dev@gmail.com>
2017-12-20 18:24:46 +0330

    Update product urls to use hashIds instead of regular ids

--- app/Product.php
+++ app/Product.php
@@ -6,10 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Mtvs\EloquentApproval\Approvable;
+use Vinkla\Hashids\Facades\Hashids;
 
 class Product extends Model
 {
     use Sluggable, Approvable, SoftDeletes;
+
+    public function getRouteKey()
+    {
+        return Hashids::encode(parent::getRouteKey());
+    }
+
+    public function resolveRouteBinding($value)
+    {
+        return parent::resolveRouteBinding(Hashids::decode($value));
+    }
 
     public function url()
     {
