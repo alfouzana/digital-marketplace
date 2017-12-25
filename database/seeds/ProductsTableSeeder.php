ea736f49aaa91c7cdc4692f99e52781131829ba5
Mohammad Ali Tavassoli <mtvs.dev@gmail.com>
2017-12-25 19:57:23 +0330

    Add products seeder

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Product;
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Product::class, 10)->create()
+            ->each(function (Product $product) {
+                $product->approve();
+            });
+    }
+}
