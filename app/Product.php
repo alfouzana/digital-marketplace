6719d5d3615e961bb86c68a08b48e32c3de9d4a6
Mohammad Ali Tavassoli <mtvs.dev@gmail.com>
2017-12-26 13:48:24 +0330

    Add products to vendors relationship

--- app/Product.php
+++ app/Product.php
@@ -41,4 +41,9 @@ class Product extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function vendor()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
