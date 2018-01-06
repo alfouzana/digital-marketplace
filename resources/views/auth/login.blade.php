1079b172e7e1fa38f73778297b1825cd8d19fcaf
Mohammad Ali Tavassoli <mtvs.dev@gmail.com>
2018-01-06 14:02:02 +0330

    Update login template
     - Add the title
     - Make it compatible with bootstrap v4
     - Make it i18n

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,62 +1,71 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+@section('title', __('Login'))
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+@section('content')
+    <div class="row">
+        <div class="col-lg-8 offset-lg-2">
+            <div class="card">
+                <div class="card-header">
+                    @lang('Login')
+                </div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                        <div class="form-group row">
+                            <label for="email" class="col-lg-4 col-form-label">
+                                @lang('E-Mail Address')
+                            </label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                            <div class="col-lg-8">
+                                <input type="email" id="email" name="email"
+                                       class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }} }}"
+                                       value="{{ old('email') }}" required autofocus>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                                <div class="invalid-feedback">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </div>
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                        <div class="form-group row">
+                            <label for="password" class="col-lg-4 col-form-label">
+                                @lang('Password')
+                            </label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                            <div class="col-lg-8">
+                                <input id="password" type="password" name="password"
+                                       class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }} }}"
+                                       required>
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                                <div class="invalid-feedback">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </div>
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                        <div class="form-group row">
+                            <div class="col-lg-8 offset-lg-4">
+                                <div class="form-check">
+                                    <input type="checkbox" name="remember" id="remember"
+                                           class="form-check-input"{{ old('remember') ? 'checked' : '' }}>
+                                    <label for="remember" class="form-check-label">
+                                        @lang('Remember Me')
                                     </label>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
+                        <div class="form-group row">
+                            <div class="col-lg-8 offset-lg-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Login
+                                    @lang('Login')
                                 </button>
 
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
+                                    @lang('Forgot Your Password?')
                                 </a>
                             </div>
                         </div>
@@ -65,5 +74,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
