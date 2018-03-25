59982769862f440155814837003873299f599a79
Mohammad Ali Tavassoli <mtvs.dev@gmail.com>
2018-03-25 13:19:36 +0430

    Restore unintended removed code

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -8,15 +8,34 @@ $factory->define(App\Product::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence,
 
-        'cover_path' => '',
+        'cover_path' => str_after(
+            $faker->image(
+                product_covers_path(),
+                640, 480,
+                'abstract'
+            ),
+            public_path().DIRECTORY_SEPARATOR
+        ),
 
         'body' => $faker->paragraph,
 
         'price' => $faker->randomFloat(2, 0, 100000000),
 
-        'sample_path' => '',
+        'sample_path' => str_after(
+            $faker->file(
+                product_covers_path(),
+                product_samples_path()
+            ),
+            public_path().DIRECTORY_SEPARATOR
+        ),
 
-        'file_path' => '',
+        'file_path' => str_after(
+            $faker->file(
+                product_covers_path(),
+                product_files_path()
+            ),
+            resource_path().DIRECTORY_SEPARATOR
+        ),
 
         'user_id' => function() {
             return factory(\App\Vendor::class)->create()->id;
