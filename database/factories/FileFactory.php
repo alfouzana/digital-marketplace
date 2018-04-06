f6645e27e0240d38b39ce868131fdc89bd0326f3
Mohammad Ali Tavassoli <mtvs.dev@gmail.com>
2018-04-06 18:54:58 +0430

    Update FileFactory states to create files when used not in testing

--- database/factories/FileFactory.php
+++ database/factories/FileFactory.php
@@ -10,14 +10,39 @@ $factory->define(\App\File::class, function (Faker $faker) {
     ];
 });
 
-$factory->state(\App\File::class, 'cover', [
-    'assoc' => 'cover'
-]);
+$factory->state(\App\File::class, 'cover', function (Faker $faker) {
+    $path = app()->environment('testing')
+        ? ''
+        : str_after(
+            $faker->image(
+                product_covers_path(),
+                640, 480,
+                'abstract'
+            ),
+            public_path().DIRECTORY_SEPARATOR
+        );
+
+    return [
+        'assoc' => 'cover',
+        'path' => $path
+    ];
+});
 
 
-$factory->state(\App\File::class, 'sample', [
-        'assoc' => 'sample'
-]);
+$factory->state(\App\File::class, 'sample', function (Faker $faker) {
+    $path = app()->environment('testing')
+        ? ''
+        : str_after(
+            \Illuminate\Http\UploadedFile::fake()->create('product-sample')
+            ->move(product_samples_path(), uniqid())->getRealPath(),
+            public_path().DIRECTORY_SEPARATOR
+        );
+
+    return [
+        'assoc' => 'sample',
+        'path' => $path
+    ];
+});
 
 //$factory->state(\App\File::class, 'main', [
 //        'assoc' => 'main'
