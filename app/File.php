2ef434d50e7a73373528e500c2f78a45a4433000
Mohammad Ali Tavassoli <mtvs.dev@gmail.com>
2018-04-06 18:51:38 +0430

    Add FilePresenter::url()

--- app/File.php
+++ app/File.php
@@ -2,9 +2,21 @@
 
 namespace App;
 
+use App\Presenters\FilePresenter;
 use Illuminate\Database\Eloquent\Model;
+use McCool\LaravelAutoPresenter\HasPresenter;
 
-class File extends Model
+class File extends Model implements HasPresenter
 {
     protected $guarded = [];
+
+    /**
+     * Get the presenter class.
+     *
+     * @return string
+     */
+    public function getPresenterClass()
+    {
+        return FilePresenter::class;
+    }
 }
---
+++ app/Presenters/FilePresenter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Presenters;
+
+
+use App\File;
+use Illuminate\Support\Facades\Storage;
+use McCool\LaravelAutoPresenter\BasePresenter;
+
+class FilePresenter extends BasePresenter
+{
+
+    /**
+     * FilePresenter constructor.
+     */
+    public function __construct(File $resource)
+    {
+        $this->wrappedObject = $resource;
+    }
+
+    public function url()
+    {
+        return asset($this->path);
+    }
+}
