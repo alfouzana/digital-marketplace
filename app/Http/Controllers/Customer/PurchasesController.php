c03ee18d9a5ea88260cd4b8e2da856d56bb57e0f
Mohammad Ali Tavassoli <mtvs.dev@gmail.com>
2018-12-02 14:28:37 +0330

    Handle the stripe's card error

--- app/Http/Controllers/Customer/PurchasesController.php
+++ app/Http/Controllers/Customer/PurchasesController.php
@@ -6,7 +6,8 @@ use App\Product;
 use App\Purchase;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Stripe\Charge;
+use Stripe\Charge as StripeCharge;
+use Stripe\Error\Card as StripCardException;
 use Vinkla\Hashids\Facades\Hashids;
 
 class PurchasesController extends Controller
@@ -21,17 +22,24 @@ class PurchasesController extends Controller
             return redirect('/');
         }
 
-        // todo: Improve product price conversion to stripe amount
-        // todo: Supprot different currencies
-        // todo: Consider max limit of stripe amount
-        Charge::create([
-            'amount' => $product->price * 100,
-            'currency' => 'usd',
-            'description' => sprintf('Purchase at "%s"', config('app.name')) ,
-            'source' => $request['stripeToken'],
-        ]);
 
-        // todo: do error handling for stripe api
+        try {
+            // todo: Improve product price conversion to stripe amount
+            // todo: Supprot different currencies
+            // todo: Consider max limit of stripe amount
+            StripeCharge::create([
+                'amount' => $product->price * 100,
+                'currency' => 'usd',
+                'description' => sprintf('Purchase at "%s"', config('app.name')) ,
+                'source' => $request['stripeToken'],
+            ]);
+        } catch (StripCardException $exception) {
+            $message = $exception->getJsonBody()['error']['message'];
+
+            flash($message)->error();
+
+            return back();
+        }
 
         auth()->user()->purchasedProducts()->attach($product->id, [
             'amount' => $product->price,
