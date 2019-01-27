da8009bb0e007897b78ca3f9552a2d6a638f1c28
Mohammad Ali Tavassoli <mtvs.dev@gmail.com>
2019-01-27 19:30:33 +0330

    Fix the test of the user purchases index

--- tests/Feature/User/Purchases/IndexTest.php
+++ tests/Feature/User/Purchases/IndexTest.php
@@ -8,6 +8,7 @@ use App\Product;
 use Illuminate\Http\Response;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Mtvs\EloquentApproval\ApprovalStatuses;
 
 class IndexTest extends TestCase
 {
@@ -16,44 +17,76 @@ class IndexTest extends TestCase
     /**
      * @test
      */
-    public function a_user_can_index_their_purchases()
+    public function an_user_can_see_their_purchases()
     {
         $user = factory(User::class)->create();
 
-        $purchases = factory(Purchase::class, 5)->create([
+        $purchases = factory(Purchase::class, 3)->create([
             'user_id' => $user->id,
         ]);
 
         $this->actingAs($user);
 
-        $response = $this->get('user/purchases');
+        $response = $this->get('/user/purchases');
 
         foreach ($purchases as $purchase) {
             $response->assertSee($purchase->hashId());
+            $response->assertSee($purchase->product->title);
         }
     }
 
     /**
      * @test
      */
-    public function an_unauthenticated_user_can_not_visit_the_customer_purchases_page()
+    public function an_user_cannot_access_their_purchased_products_that_are_not_approved_anymore()
     {
-        $this->withExceptionHandling();
+        $user = factory(User::class)->create();
 
-        $this->get('customer/purchases')->assertRedirect('login');
+        $purchases[] = factory(Purchase::class)->create([
+            'user_id' => $user->id,
+            'product_id' => with($products[] = factory(Product::class)->create([
+                'approval_status' => ApprovalStatuses::PENDING,
+            ]))->id
+        ]);
+
+        $purchases[] = factory(Purchase::class)->create([
+            'user_id' => $user->id,
+            'product_id' => with($products[] = factory(Product::class)->create([
+                'approval_status' => ApprovalStatuses::REJECTED,
+            ]))->id
+        ]);
+
+        $this->actingAs($user);
+
+        
+        foreach (range(0, 1) as $i) {
+            $this->get('/user/purchases')
+            ->assertSee($purchases[$i]->hashId())
+            ->assertDontSee($products[$i]->title);
+        }
     }
 
     /**
      * @test
      */
-    public function a_non_customer_user_can_not_visit_the_customer_purchases_page()
+    public function an_unauthenticated_user_cannot_visit_the_user_purchases_page()
+    {
+        $this->withExceptionHandling();
+
+        $this->get('/user/purchases')->assertRedirect('login');
+    }
+
+    /**
+     * @test
+     */
+    public function an_admin_cannot_visit_the_user_purchases_page()
     {
         $this->withExceptionHandling();
 
         $this->actingAs(
-            $this->createNonCustomerUser()
+            factory(User::class)->states('admin')->create()
         );
 
-        $this->get('customer/purchases')->assertStatus(Response::HTTP_FORBIDDEN);
+        $this->get('/user/purchases')->assertStatus(Response::HTTP_FORBIDDEN);
     }
 }
